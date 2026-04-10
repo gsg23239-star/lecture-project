b6a9ab1459ed4968b5120a0177d7af981755921c
LINE <gsg2323@naver.com>
2026-04-10 18:13:06 +0900

    라우팅 css 변경

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -11,7 +11,13 @@ function MovieCard({ poster, title, overview, vote_average }) {
                 alt={"POSTER"}
             />
             <div className={"styles.movie-Content"}>
-                <h2 className={styles.movieTitle}>{title}</h2>
+                {/*
+                    라액트 내부에서 이동을 시킬 대에는 a 태그를 사용하지 말고 Link 기능 사용해야함
+                    a태그는 href를 통해 이동시킬 주소를 기재하지만,
+                    Link는 to를 통해 이동시킬 주소를 기재함.
+                    당연히 외부로 이동 시킬때 (예. 네이버) 할때는 a 태그를 써줘야함
+                */}
+                <Link className={styles.movieTitle} to={"/abc"}>{title}</Link>
                 <h5 className={styles.movieVoteAverage}>{vote_average}</h5>
                 <h6 className={styles.movieOverview}>{overview}</h6>
             </div>
---
+++ src/routes/Detail.jsx
@@ -0,0 +1,10 @@
+import { useParams } from "react-router";
+import { useState } from "react";
+
+function Detail() {
+    const { id} = useParams();
+
+    return <>Detail</>;
+}
+
+export default Detail;
